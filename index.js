3632f5d7979d3fee0bab177e2f669cf7c83b4944
Mara Hernandez <mara.hesp@gmail.com>
2022-09-26 16:37:52 -0500

    first-commit

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,13 +14,41 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+  let date = req.params.date;
+  console.log(date)
+  let regex1 = new RegExp(/-/, 'g')
+  let regex2 = new RegExp(/[a-z]/, 'g')
+  let newDate = {}
+  
+  if (regex1.test(date) || regex2.test(date)) {
+    newDate['unix'] = new Date(date).getTime()
+    newDate['utc'] = new Date(date).toUTCString()
+  } else {
+      newDate['unix'] = new Date(parseInt(date)).getTime()
+    	newDate['utc'] = new Date(parseInt(date)).toUTCString()
+  }
+  if (!newDate['unix'] || !newDate['utc']) {
+    delete newDate.unix
+    delete newDate.utc
+    newDate['error'] = "Invalid Date"
+  }
+  res.json(newDate)
 });
+
+app.get("/api/", (req, res) => {
+  let now = new Date()
+  let newDate = {unix: "", utc: ""}
+  newDate.unix = now.getTime()
+  newDate.utc = now.toUTCString()
+  res.json(newDate)
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`El servidor está escuchando en el puerto ${PUERTO}...`)
 });
